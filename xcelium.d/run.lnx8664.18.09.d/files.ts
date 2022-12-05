ca3cbcd67d23d58e897893c47f4141c1ee8c8fb0
Kinsey Pak Yin Ho <kph3798@joker.ece.northwestern.edu>
2022-12-04 23:19:58 -0600

    fixing files

--- xcelium.d/run.lnx8664.18.09.d/files.ts
+++
@@ -1,4 +0,0 @@
-1670192221 /home/kph3798/ce361/single_cycle_processor/lib/sign_ext_30.v
-1670215095 /home/kph3798/ce361/single_cycle_processor/lib/full_adder_30.v
-1670217301 /home/kph3798/ce361/single_cycle_processor/src/instruction_fetch.v
-1670217287 /home/kph3798/ce361/single_cycle_processor/test/instr_fetch_test.v
