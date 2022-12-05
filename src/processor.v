f5b8a3dfaabdaf41aa11c37c1ebc3247271370a0
haolanzhan <haolanzhan@gmail.com>
2022-12-04 23:26:48 -0600

    small changes to processor

--- src/processor.v
+++ src/processor.v
@@ -11,25 +11,30 @@ module processor (clk);
 	input clk;
 
 	//Control signals for ifetch (control unit->instruction fetch)
-	wire nPC_sel; 
+	wire nPC_sel, Branch;
 
 	//Control signals for datapath (control unit->datapath)
+	wire RegWr, RegDst, ExtOp, AluSrc, MemWr, MemtoReg, ALUctr, 
 
 	//feedback signals for control path (datapath->control unit)
+	wire zero, msb;
 
 	//instruction (ifetch -> control unit and datapath)
 	wire [31:0] instruction; 
+
+	//signals for alu control
+	wire ALUop;
 
 
 	//instantiate instruction fetch
 	instruction_fetch ifetch (d, clk, nPC_sel, instruction); 
 
 	//instantiate control unit
-	main_control ctrl(/*Opcode*/, Zero, AluResMsb, nPC_sel, RegWr, RegDst, ExtOp, AluSrc, AluOp, MemWr, MemtoReg, Branch);
-	alu_control aluctrl(ALUop, func, ALUctr);
+	main_control ctrl(/*Opcode*/instruction[31:26], zero, msb, nPC_sel, RegWr, RegDst, ExtOp, AluSrc, AluOp, MemWr, MemtoReg, Branch);
+	alu_control aluctrl(AluOp, /*func*/instruction[5:0], ALUctr);
 
 	//instantiate datapath
-	datapath dpath (clk, instruction, regwrite, regdst, extop, alusrc, memwrite, mem2reg, aluctrl, zero, msb);
+	datapath dpath (clk, instruction, RegWr, RegDst, ExtOp, AluSrc, MemWr, MemtoReg, ALUctr, zero, msb);
 
 
 endmodule
