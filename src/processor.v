95b53c9502368afefef25872e8516630c58c4617
haolanzhan <haolanzhan@gmail.com>
2022-12-05 03:43:55 -0600

    testing

--- src/processor.v
+++ src/processor.v
@@ -5,7 +5,7 @@
 `include "src/alu_control.v"
 `include "src/datapath.v"
 
-module processor (clk, start_up);
+module processor (clk, start_up, instruction, busW, aluresult);
   	parameter program1 = ""; //program
 	input clk, start_up;
 
@@ -13,16 +13,19 @@ module processor (clk, start_up);
 	wire nPC_sel, Branch;
 
 	//Control signals for datapath (control unit->datapath)
-	wire RegWr, RegDst, ExtOp, AluSrc, MemWr, MemtoReg, ALUctr;
+	wire RegWr, RegDst, ExtOp, AluSrc, MemWr, MemtoReg, ALUctr, SllFlag;
 
 	//feedback signals for control path (datapath->control unit)
 	wire zero, msb;
 
 	//instruction (ifetch -> control unit and datapath)
-	wire [31:0] instruction; 
+	output [31:0] instruction; 
 
 	//signals for alu control
 	wire ALUop;
+
+	//test outputs
+    output  [31:0]  busW, aluresult;
 
 	//instantiate instruction fetch (in: d ... nPC_sel; out: instruction)
 	instruction_fetch #(.program2(program1)) ifetch (start_up, clk, nPC_sel, instruction); 
@@ -30,8 +33,8 @@ module processor (clk, start_up);
 	//instantiate control unit (in: Instruction ... msb; out: nPC_sel ... Branch)
 	main_control ctrl(/*Opcode*/instruction[31:26], zero, msb, nPC_sel, RegWr, RegDst, ExtOp, AluSrc, AluOp, MemWr, MemtoReg, Branch);
 	//(in: Aluop, Instruction; out: ALUctr)
-	alu_control aluctrl(AluOp, /*func*/instruction[5:0], ALUctr);
+	alu_control aluctrl(AluOp, /*func*/instruction[5:0], ALUctr, SllFlag);
 
 	//instantiate datapath (in: clk ... ALUctr; out: zero, msb)
-	datapath #(.file(program1)) dpath (clk, instruction, RegWr, RegDst, ExtOp, AluSrc, MemWr, MemtoReg, ALUctr, zero, msb);
+	datapath #(.file(program1)) dpath (clk, instruction, RegWr, RegDst, ExtOp, AluSrc, MemWr, MemtoReg, ALUctr, SllFlag, zero, msb, busW, aluresult);
 endmodule
--- test/processor_test.v
+++ test/processor_test.v
@@ -7,13 +7,15 @@ module processor_test;
   	reg clk_tb, start_up_tb;
 
 	//output
+	wire  [31:0]  busW_tb, aluresult_tb, instruction_tb;
 
-    initial $monitor ("clk_tb: %b start_up_tb: %b", 
-                    clk_tb, start_up_tb);
+
+    initial $monitor ("clk_tb: %b start_up_tb: %b instruction_tb: %b busW_tb: %b aluresult_tb: %b", 
+                    clk_tb, start_up_tb, instruction_tb, busW_tb, aluresult_tb);
 	 
 	always #5 clk_tb = ~clk_tb;
 
-	processor #(.program1("eecs361/data/unsigned_sum.dat")) better_than_mips (clk_tb, start_up_tb);
+	processor #(.program1("eecs361/data/unsigned_sum.dat")) better_than_mips (clk_tb, start_up_tb, instruction_tb, busW_tb, aluresult_tb);
 
 	initial 
 		begin
